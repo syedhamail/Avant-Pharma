195d27ff472ec6833155fd580041d82f61230242
@syedhamailmuq <hamailsyed139@gmail.com>
2025-04-25 18:22:35 +0500

    first commit

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    siteUrl: 'https://www.avantpharmaceutical.com.pk',
+    generateRobotsTxt: true,
+  };
+  
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Avant Pharmaceuticals",
-  description: "Avant Pharmaceuticals Official Website",
+  title: "Avant Pharmaceuticals | Best Pharma in Pakistan",
+  description: "Avant Pharma has been operating in Pakistan for more than 11 years. Over this period, the company has successfully developed and strengthened its pharmaceutical ...",
 };
 
 export default function RootLayout({
@@ -17,6 +17,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* OG tags for social and search preview */}
+        <meta property="og:title" content="Avant Pharmaceuticals" />
+        <meta property="og:description" content="Top pharma company offering healthcare solutions in Pakistan." />
+        <meta property="og:image" content="/Avant-Website-Photos/avant-logo.png" />
+        <meta property="og:url" content="https://www.avantpharmaceutical.com.pk/" />
+        <meta name="twitter:card" content="summary_large_image" />
         <link
           rel="icon"
           href="/Avant-Website-Photos/avant-logo_title.ico"
